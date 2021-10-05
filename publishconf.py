f5db6f120f15b321b26b7dafa54f459e8b7f87d1
Chase-Warwick <warwick@ualberta.ca>
2021-10-05 14:10:48 -0600

    Added my own theme

--- publishconf.py
+++ publishconf.py
@@ -10,13 +10,13 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://chase-warwick.github.io/CMPUT404-Lab-5/'
+SITEURL = 'https://chase-warwick.github.io/CMPUT404-Lab-5'
 RELATIVE_URLS = False
 
 OUTPUT_PATH = "docs/"
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+#CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
